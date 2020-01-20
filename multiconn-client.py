f5678dfa39930420ca4794bd0618714c32a9a407
Ryan J Slater <ryan.j.slater.2@gmail.com>
2020-01-20 12:34:41 -0500

    Made multiconnection actually work

--- multiconn-client.py
+++ multiconn-client.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
 import socket
+from _thread import *
+import threading
+from sys import argv
 
+def listenerThread(s):
+    while True:
+        data = s.recv(1024)
+        print('HOST:', str(data.decode('ascii')))
 
 def main():
     host = '130.108.202.158'
-    port = 12345
+    port = 65432 if len(argv) < 2 else int(argv[1])
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
 
-    message = 'message from client'
+    start_new_thread(listenerThread, (s,))
+
     while True:
-        s.send(message.encode('ascii'))
-        data = s.recv(1024)
-        print('Received:', str(data.decode('ascii')))
-        if input('\nDo you want to continue(y/n) :') == 'y':
-            continue
-        else:
-            break
+        s.send(input('Enter message to send:').encode('ascii'))
     s.close()
 
 
--- multiconn-host.py
+++ multiconn-host.py
@@ -3,45 +3,88 @@
 import socket
 from _thread import *
 import threading
+import traceback
 
 printLock = threading.Lock()
+connections = []
 
 
-def listenerThread(c):
+def listenerThread(connData):
+    global connections
+    global sendQueue
+
+    c, addr = connData
+
     while True:
         # Receive data
-        data = c.recv(1024)
-
-        # If can't receive data, kill process
-        if not data:
-            print('Bye')
-            printLock.release()
+        try:
+            data = c.recv(1024)
+            if not data:
+                raise Exception('crap')
+        except:
+            print('Bye {}!'.format(addr[0]))
+            c.close()
+            printLock.acquire()
+            connections.remove(connData)
+            printLock.release
             break
 
-        # Send data back to client, but reversed
-        data = data[::-1]
-        c.send(data)
-    c.close()
+        # Print message
+        print(addr[0], ':', data.decode('utf-8'))
 
+        # Update the data to send
+        sendQueue.append(data)
+
+        # Send bulk message to everyone connnected
+        printLock.acquire()
+        sendData = sendQueue.pop()
+        for connection in connections:
+            connection[0].send(sendData)
+        printLock.release()
 
 def main():
-    host = '130.108.202.158'
-    port = 12345
+    global connections
+    global sendQueue
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print('Socket bound to port', port)
+    host = '130.108.202.158'
+    port = 65432
+
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((host, port))
+            print('Socket bound to port', port)
+            break
+        except:
+            print('Failed to start host')
+            port += 1
+            print('Trying to listen on port {}'.format(port))
+    print()
 
     s.listen(5)
 
-    while True:
-        c, addr = s.accept()
+    connections = []
+    sendQueue = []
 
-        printLock.acquire()
-        print('Connected to :', addr[0], ':', addr[1])
+    try:
+        while True:
+            c, addr = s.accept()
 
-        start_new_thread(listenerThread, (c,))
-    s.close()
+            connData = (c, addr)
+            printLock.acquire()
+            connections.append(connData)
+            printLock.release()
+
+            print('Connections:', end=' ')
+            for conn in connections:
+                print(conn[1][0], end=' ')
+            print()
+
+            start_new_thread(listenerThread, (connData,))
+            # c.sendall(b'bugger off')
+    except:
+        print('Host crashed (fatal error in while loop)')
+        s.close()
 
 
 if __name__ == '__main__':
